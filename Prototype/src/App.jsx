76c327958808290cdc84def6778c6b8921200e4b
Elite Carlson <elitecarlson@gmail.com>
2022-10-08 17:10:59 +0200

    React Template final

---
+++ Prototype/src/App.jsx
@@ -0,0 +1,16 @@
+import "../src/Assets/css/app.css";
+// import icon from "../src/Assets/images/icon.jpg";
+function App() {
+  return (
+    <div className="App">
+      <>
+      <div className="imgcont">
+        <div className="img"></div>
+        {/* <img src={icon} alt="" /> */}
+      </div>
+      </>
+    </div>
+  );
+}
+
+export default App;
